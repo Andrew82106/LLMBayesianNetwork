6b30913d0f02c8f87355cb456a09d894091eb76d
Andrew82106 <53038854+Andrew82106@users.noreply.github.com>
2025-04-27 12:26:54 +0800

    find best

--- misc.py
+++ misc.py
@@ -1,14 +1,60 @@
 from datetime import datetime
 import pickle
 from MainTools.pthcfg import *
-from MainTools.DrawGraph import visualize_bayesian_network
-
 pthcfg = PathConfig()
-
+from MainTools.DrawGraph import visualize_bayesian_network
+from MainTools.utils import calc_graph_f1
 from MainTools.utils import readOutputPkl
 
-pklData = readOutputPkl(os.path.join(pthcfg.final_output_path, "ExpertResult_labData_time_2025-04-2511_00_51.pkl"))
+pklRouteList = os.listdir(pthcfg.final_output_path)
+ExpertList = []
+llmList = []
+
+for pklRoute in pklRouteList:
+    if "Expert" in pklRoute:
+        ExpertList.append(pklRoute)
+    elif "labData" in pklRoute:
+        llmList.append(pklRoute)
+
+
+print(ExpertList)
+print(llmList)
+maxF1 = 0
+maxExpertRoute = None
+maxLLMRoute = None
+
+for ExpertRoute in ExpertList:
+    for llmRoute in llmList:
+        ExpertPKL = readOutputPkl(os.path.join(pthcfg.final_output_path, ExpertRoute))
+        G1 = ExpertPKL['result']['Graph']['Victimhuman'][1]
+        llmPKL = readOutputPkl(os.path.join(pthcfg.final_output_path, llmRoute))
+        G2 = llmPKL['result']['Graph']['Victimllm'][1]
+        F1, P, R = calc_graph_f1(G1.edges(), G2.edges())
+        if F1 > maxF1:
+            maxF1 = F1
+            maxExpertRoute = ExpertRoute
+            maxLLMRoute = llmRoute
+
+
+print(maxExpertRoute)
+print(maxLLMRoute)
+
+pklData = readOutputPkl(os.path.join(pthcfg.final_output_path, maxExpertRoute))
 
 print(type(pklData['result']['Graph']['Victimhuman'][1]))
 
 visualize_bayesian_network(pklData['result']['Graph']['Victimhuman'][1], os.path.join(pthcfg.figure_output_path, "Victimhuman.png"))
+
+G1 = pklData['result']['Graph']['Victimhuman'][1]
+
+pklData = readOutputPkl(os.path.join(pthcfg.final_output_path, maxLLMRoute))
+
+print(type(pklData['result']['Graph']['Victimllm'][1]))
+
+visualize_bayesian_network(pklData['result']['Graph']['Victimllm'][1], os.path.join(pthcfg.figure_output_path, "Victimllm.png"))
+
+G2 = pklData['result']['Graph']['Victimllm'][1]
+
+print(pklData['parameter'])
+
+print(calc_graph_f1(G1.edges(), G2.edges()))
