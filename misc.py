ccaa1a1aaba4bc9be5e9faab7f44255b611a682a
Andrew82106 <53038854+Andrew82106@users.noreply.github.com>
2025-04-28 09:20:23 +0800

    finish lab

--- misc.py
+++ misc.py
@@ -26,9 +26,9 @@ maxLLMRoute = None
 for ExpertRoute in ExpertList:
     for llmRoute in llmList:
         ExpertPKL = readOutputPkl(os.path.join(pthcfg.final_output_path, ExpertRoute))
-        G1 = ExpertPKL['result']['Graph']['Victimhuman'][1]
+        G1 = ExpertPKL['result']['Graph']['Criminalhuman'][1]
         llmPKL = readOutputPkl(os.path.join(pthcfg.final_output_path, llmRoute))
-        G2 = llmPKL['result']['Graph']['Victimllm'][1]
+        G2 = llmPKL['result']['Graph']['Criminalllm'][1]
         F1, P, R = calc_graph_f1(G1.edges(), G2.edges())
         if F1 > maxF1:
             maxF1 = F1
@@ -38,23 +38,32 @@ for ExpertRoute in ExpertList:
 
 print(maxExpertRoute)
 print(maxLLMRoute)
-
 pklData = readOutputPkl(os.path.join(pthcfg.final_output_path, maxExpertRoute))
 
-print(type(pklData['result']['Graph']['Victimhuman'][1]))
+print(type(pklData['result']['Graph']['Criminalhuman'][1]))
 
-visualize_bayesian_network(pklData['result']['Graph']['Victimhuman'][1], os.path.join(pthcfg.figure_output_path, "Victimhuman.png"))
+visualize_bayesian_network(pklData['result']['Graph']['Criminalhuman'][1], os.path.join(pthcfg.figure_output_path, "Criminalhuman.png"))
 
-G1 = pklData['result']['Graph']['Victimhuman'][1]
+G1 = pklData['result']['Graph']['Criminalhuman'][1]
 
 pklData = readOutputPkl(os.path.join(pthcfg.final_output_path, maxLLMRoute))
 
-print(type(pklData['result']['Graph']['Victimllm'][1]))
+print(type(pklData['result']['Graph']['Criminalllm'][1]))
 
-visualize_bayesian_network(pklData['result']['Graph']['Victimllm'][1], os.path.join(pthcfg.figure_output_path, "Victimllm.png"))
+visualize_bayesian_network(pklData['result']['Graph']['Criminalllm'][1], os.path.join(pthcfg.figure_output_path, "Criminalllm.png"))
 
-G2 = pklData['result']['Graph']['Victimllm'][1]
+G2 = pklData['result']['Graph']['Criminalllm'][1]
 
 print(pklData['parameter'])
 
 print(calc_graph_f1(G1.edges(), G2.edges()))
+
+
+ExpertBayesianNetworkCriminal = readOutputPkl(os.path.join(pthcfg.final_output_path, "ExpertBayesianNetworkCriminal.pkl"))
+visualize_bayesian_network(ExpertBayesianNetworkCriminal['result']['Graph']['Criminalhuman'][1], os.path.join(pthcfg.figure_output_path, "Criminalhuman.png"))
+ExpertBayesianNetworkVictim = readOutputPkl(os.path.join(pthcfg.final_output_path, "ExpertBayesianNetworkVictim.pkl"))
+visualize_bayesian_network(ExpertBayesianNetworkVictim['result']['Graph']['Victimhuman'][1], os.path.join(pthcfg.figure_output_path, "Victimhuman.png"))
+LLMBayesianNetworkCriminal = readOutputPkl(os.path.join(pthcfg.final_output_path, "LLMBayesianNetworkCriminal.pkl"))
+visualize_bayesian_network(LLMBayesianNetworkCriminal['result']['Graph']['Criminalllm'][1], os.path.join(pthcfg.figure_output_path, "Criminalllm.png"))
+LLMBayesianNetworkVictim = readOutputPkl(os.path.join(pthcfg.final_output_path, "LLMBayesianNetworkVictim.pkl"))
+visualize_bayesian_network(LLMBayesianNetworkVictim['result']['Graph']['Victimllm'][1], os.path.join(pthcfg.figure_output_path, "Victimllm.png"))
